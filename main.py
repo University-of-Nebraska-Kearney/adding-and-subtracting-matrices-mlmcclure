10a99be8040e95a317f8567f9e383e3c5fc53514
Melodie McClure <melod@mac.unk.edu>
2026-01-21 19:03:49 -0600

    add print and testing things

--- main.py
+++ main.py
@@ -23,6 +23,27 @@ def add_matrix(matrix1, matrix2):
         result_matrix.append(row)
 
     return result_matrix
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+def main():
+    m1 = get_matrix()
+    print_matrix(m1)
+    print()
+
+    m2 = get_matrix()
+    print_matrix(m2)
+    print()
+
+    m3 = add_matrix(m1, m2)
+    print_matrix(m3)
+    print()
+
+
+if __name__ == "__main__":
+    main()
 #get_martix(): creates and returns a matrix from user input such that the size and dimension is determined by the user. This should be a hybrid of the fixed-sized and the sentinel model we discussed in class. The user should be prompted for the number or rows and columns before entering values to fill the matrix.
 
 #add_matrix(matrix1, matrix2): takes two matrices as parameters, verifies that they can be added together, adds them together, and returns the sum.
