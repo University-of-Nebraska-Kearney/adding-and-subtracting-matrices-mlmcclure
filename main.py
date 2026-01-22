656b29cca0c4b79085adb46bb0c1f1dfc367a025
Melodie McClure <melod@mac.unk.edu>
2026-01-21 18:56:41 -0600

    add `add_matrix`

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ def get_matrix():
         matrix.append(row)
     return matrix
 
+def add_matrix(matrix1, matrix2):
+    assert len(matrix1) == len(matrix2)
+    assert len(matrix1[0]) == len(matrix2[0])
 
+    result_matrix = []
+    for row1, row2 in zip(matrix1, matrix2, strict = True):
+        row = []
+        for element1, element2 in zip(row1, row2, strict = True):
+            row.append(element1 + element2)
+        result_matrix.append(row)
 
-
-
-
-
-
-
-
+    return result_matrix
 #get_martix(): creates and returns a matrix from user input such that the size and dimension is determined by the user. This should be a hybrid of the fixed-sized and the sentinel model we discussed in class. The user should be prompted for the number or rows and columns before entering values to fill the matrix.
 
 #add_matrix(matrix1, matrix2): takes two matrices as parameters, verifies that they can be added together, adds them together, and returns the sum.
